0b6f6803d17f1cf0de7444050083fe1d641850ed
Tony Tran <toant13@users.noreply.github.com>
2019-01-27 20:56:09 -0500

    Quad 842 (#3)
    * fetch ohlcv
    
    * Load markets
    
    * Fetch trades
    
    * Orderbook snapshot
    
    * Added fetch balances
    
    * fetch closed orders
    
    * Update cancel order
    
    * Fixed create order function
    
    * Fetch my trades
    
    * Remove test file
    
    * Ticker subscription
    
    * Subscribe trades
    
    * Added orderbook
    
    * subscribe to balance

--- README.md
+++ README.md
@@ -1,2 +1,49 @@
 # quad-kucoin-client
 node client for kucoin V2 api
+
+
+Client to allow easier access to Kucoin's V@ rest and websocket api.
+
+\* There are a handlful of assets that have been normalized to ccxt's library. 
+
+## Rest Example
+
+```
+import kucoin from 'quad-kucoin-client';
+
+// For authenticated endpoints, provide object of credentials.  
+// This is not required for public rest endpoints
+const exchangeClient = new kucoin.RestClient({
+  apiKey: 'your api key',
+  secret: 'your secret',
+  password: 'your passphrase',
+});
+
+// if using a proxy, set proxy value
+if (PROXY_HOST) {
+  exchangeClient.proxy = 'http://yourproxyhost';
+}
+const allMyOrderForBTCUSDT = await exchangeClient.fetchOrders('BTC-USDT')
+
+```
+
+## Websocket Example
+
+```
+import kucoin from 'quad-kucoin-client';
+
+// For authenticated endpoints, provide object of credentials.  
+// This is not required for public rest endpoints
+const exchangeClient = new kucoin.WebsocketClient({
+  apiKey: 'your api key',
+  secret: 'your secret',
+  password: 'your passphras',
+});
+
+exchangeClient.subscribeBalance((balanceUpdate, disconnectFn)=>{
+  console.log('My balance update:', balanceUpdate);
+  
+  setTimeout(() => { disconnectFn(); }, 5000);
+});
+
+```
---
+++ src/websocket.js
@@ -0,0 +1,269 @@
+import WebSocket from 'ws';
+import axios from 'axios';
+import CryptoJS from 'crypto-js';
+import { COMMON_CURRENCIES, uuidv4 } from './utils';
+import KucoinRest from './api';
+
+const EXCHANGE = 'KUCOIN';
+const BASE_URL = 'https://openapi-v2.kucoin.com';
+
+class KucoinWebsocket {
+  constructor(userConfig = {}) {
+    Object.keys(userConfig).forEach((key) => {
+      this[key] = userConfig[key];
+    });
+
+    this.proxy = '';
+
+    this.urls = {
+      api: BASE_URL,
+    };
+    this.restClient = new KucoinRest();
+  }
+
+  async loadMarketCache() {
+    if (!this.restClient.markets) {
+      this.restClient.urls.api = this.urls.api;
+      await this.restClient.loadMarkets(true);
+    }
+  }
+
+  async getPublicServer() {
+    const bulletPath = '/api/v1/bullet-public';
+    const options = {
+      method: 'POST',
+      url: `${this.proxy}${this.urls.api}${bulletPath}`,
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    };
+    const response = await axios(options);
+    return response.data.data;
+  }
+
+  async getPrivateServer() {
+    const uri = '/api/v1/bullet-private';
+    const method = 'POST';
+    const timestamp = Date.now();
+    const sign = CryptoJS.enc.Base64.stringify(CryptoJS.HmacSHA256(`${timestamp}${method}${uri}`, this.secret));
+
+    const options = {
+      method,
+      url: `${this.proxy}${this.urls.api}${uri}`,
+      headers: {
+        'Content-Type': 'application/json',
+        'KC-API-KEY': this.apiKey,
+        'KC-API-SIGN': sign,
+        'KC-API-TIMESTAMP': timestamp,
+        'KC-API-PASSPHRASE': this.password,
+      },
+    };
+    const response = await axios(options);
+    return response.data.data;
+  }
+
+
+  subscribePublic(subscription, callback) {
+    this.getPublicServer().then((server) => {
+      const connectionId = uuidv4();
+      const { token, instanceServers } = server;
+      const { endpoint, pingInterval: pingIntervalLength } = instanceServers[0];
+
+      const socket = new WebSocket(`${endpoint}?token=${token}&[connectId=${connectionId}]`);
+      let pingInterval;
+
+      socket.onopen = () => {
+        console.log(`${EXCHANGE} connection open`);
+
+        callback({ subject: 'socket.open' }, () => { socket.close(); });
+        const subscriptionWithId = Object.assign({ id: connectionId }, subscription);
+        socket.send(JSON.stringify(subscriptionWithId));
+
+        pingInterval = setInterval(() => {
+          if (socket.readyState === socket.OPEN) {
+            const pingMessage = {
+              id: connectionId,
+              type: 'ping',
+            };
+            socket.send(JSON.stringify(pingMessage));
+          }
+        }, pingIntervalLength / 2);
+      };
+
+      socket.onmessage = (message) => {
+        const messageObj = JSON.parse(message.data);
+
+        const { type } = messageObj;
+        if (type === 'message') {
+          callback(messageObj, () => { socket.close(); });
+        }
+      };
+
+      socket.onclose = () => {
+        console.log(`${EXCHANGE} connection closed`);
+        clearInterval(pingInterval);
+      };
+
+      socket.onerror = (error) => {
+        console.log(`error with ${EXCHANGE} connection because`, error);
+        this.subscribePublic(subscription, callback);
+      };
+    });
+  }
+
+  subscribePrivate(subscription, callback) {
+    this.getPrivateServer().then((server) => {
+      const connectionId = uuidv4();
+      const { token, instanceServers } = server;
+      const { endpoint, pingInterval: pingIntervalLength } = instanceServers[0];
+
+      const socket = new WebSocket(`${endpoint}?token=${token}&[connectId=${connectionId}]`);
+      let pingInterval;
+
+      socket.onopen = () => {
+        console.log(`${EXCHANGE} connection open`);
+
+        callback({ subject: 'socket.open' }, () => { socket.close(); });
+        const subscriptionWithId = Object.assign({ id: connectionId }, subscription);
+        socket.send(JSON.stringify(subscriptionWithId));
+
+        pingInterval = setInterval(() => {
+          if (socket.readyState === socket.OPEN) {
+            const pingMessage = {
+              id: connectionId,
+              type: 'ping',
+            };
+            socket.send(JSON.stringify(pingMessage));
+          }
+        }, pingIntervalLength / 2);
+      };
+
+      socket.onmessage = (message) => {
+        const messageObj = JSON.parse(message.data);
+
+        const { type } = messageObj;
+        if (type === 'message') {
+          callback(messageObj, () => { socket.close(); });
+        }
+      };
+
+      socket.onclose = () => {
+        console.log(`${EXCHANGE} connection closed`);
+        clearInterval(pingInterval);
+      };
+
+      socket.onerror = (error) => {
+        console.log(`error with ${EXCHANGE} connection because`, error);
+        this.subscribePrivate(subscription, callback);
+      };
+    });
+  }
+
+  subscribeTickers(pairs, callback) {
+    this.loadMarketCache().then(() => {
+      const subscriptionPairsArray = !pairs || !pairs.length
+        ? (Object.keys(this.restClient.markets)).map(pair => this.restClient.markets[pair].symbol)
+        : pairs.map(pair => this.restClient.markets[pair].symbol);
+
+      const subscription = {
+        type: 'subscribe',
+        topic: `/market/ticker:${subscriptionPairsArray.toString()}`,
+        response: true,
+      };
+      this.subscribePublic(subscription, (message, disconnect) => {
+        const { subject, data, topic } = message;
+        if (subject === 'socket.open') {
+          callback({ messageType: 'open' }, disconnect);
+        }
+
+        if (subject === 'trade.ticker') {
+          const subscriptionPair = topic.replace('/market/ticker:', '');
+          const normalizedPair = KucoinRest.normalizePair(subscriptionPair);
+
+          callback(Object.assign({ messageType: 'message', pair: normalizedPair }, data), disconnect);
+        }
+      });
+    });
+  }
+
+  subscribeTrades(pairs, callback) {
+    this.loadMarketCache().then(() => {
+      const subscriptionPairsArray = !pairs || !pairs.length
+        ? (Object.keys(this.restClient.markets)).map(pair => this.restClient.markets[pair].symbol)
+        : pairs.map(pair => this.restClient.markets[pair].symbol);
+
+      const subscription = {
+        type: 'subscribe',
+        topic: `/market/match:${subscriptionPairsArray.toString()}`,
+        response: true,
+      };
+      this.subscribePublic(subscription, (message, disconnect) => {
+        const { subject, data } = message;
+        if (subject === 'socket.open') {
+          callback({ messageType: 'open' }, disconnect);
+          return;
+        }
+
+        if (subject === 'trade.l3match') {
+          const normalizedPair = KucoinRest.normalizePair(data.symbol);
+          const payload = Object.assign({ messageType: 'message', pair: normalizedPair }, data);
+          delete payload.symbol;
+          callback(payload, disconnect);
+        }
+      });
+    });
+  }
+
+  subscribeOrderbook(pairs, callback) {
+    this.loadMarketCache().then(() => {
+      const subscriptionPairsArray = !pairs || !pairs.length
+        ? (Object.keys(this.restClient.markets)).map(pair => this.restClient.markets[pair].symbol)
+        : pairs.map(pair => this.restClient.markets[pair].symbol);
+
+      const subscription = {
+        type: 'subscribe',
+        topic: `/market/level2:${subscriptionPairsArray.toString()}`,
+        response: true,
+      };
+      this.subscribePublic(subscription, (message, disconnect) => {
+        const { subject, data } = message;
+        if (subject === 'socket.open') {
+          callback({ messageType: 'open' }, disconnect);
+          return;
+        }
+
+        if (subject === 'trade.l2update') {
+          const normalizedPair = KucoinRest.normalizePair(data.symbol);
+          const payload = Object.assign({ messageType: 'message', pair: normalizedPair }, data);
+          delete payload.symbol;
+          callback(payload, disconnect);
+        }
+      });
+    });
+  }
+
+  subscribeUserBalance(callback) {
+    const subscription = {
+      type: 'subscribe',
+      topic: '/account/balance',
+      privateChannel: true,
+      response: true,
+    };
+    this.subscribePrivate(subscription, (message, disconnect) => {
+      const { subject, data } = message;
+      if (subject === 'socket.open') {
+        callback({ messageType: 'open' }, disconnect);
+        return;
+      }
+
+      if (subject === 'account.balance') {
+        const payload = Object.assign({ messageType: 'message' }, data);
+        payload.currency = COMMON_CURRENCIES[payload.currency] ? COMMON_CURRENCIES[payload.currency] : payload.currency;
+        callback(payload, disconnect);
+      }
+    });
+  }
+}
+
+
+export default KucoinWebsocket;
